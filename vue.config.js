8a7ee2baedef1c14657a9f4f336f963aa27d9b41
汤兴平 <tangxingping@qjdchina.com>
2023-09-05 17:10:22 +0800

    配置修改

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  devServer: { disableHostCheck: true }
+  devServer: {
+    open: true,//启动项目后自动开启浏览器
+    historyApiFallback: true,
+    allowedHosts: "all",
+    // host: 'localhost',//对应的主机名
+    port: 8080,//端口号
+    https: false,//是否开启协议名,如果开启会发出警告
+  }
 })
