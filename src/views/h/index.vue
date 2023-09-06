a9eb25587c13e152a21c9adb05fd85a96c4b29b9
汤兴平 <tangxingping@qjdchina.com>
2023-09-06 15:10:13 +0800

    增加注释

--- src/views/h/index.vue
+++ src/views/h/index.vue
@@ -18,13 +18,21 @@ import Title from './components/title.vue'
 import Content from './components/content.js'
 Vue.component('Hcomp', {
   render: function (h) {
+    // 打印vnode，查看结构
     console.log(this.$slots)
+
+    // 为了说明vnode可以灵活运用，这里分类组件进行渲染
     const titleComponent = this.$slots.default[0]
     const descComponent = this.$slots.default[1]
+
+    /**
+     * h函数接受的参数不同的使用方式接受不同
+     * 这里结合渲染vnode，只传两个参数，参数1：容器；参数2：vnode
+     */
     return h(
       'div',   // 标签名称
       [
-      titleComponent,
+        titleComponent,
         h(
           'p', 
           [descComponent]
--- src/views/jsx/components/content.js
+++ src/views/jsx/components/content.js
@@ -4,23 +4,33 @@ export default {
   data() {
     return {};
   },
-  render(h) {
+  render() {
+
     // 获取vnode
     const slot = this.$slots.default;
+
     // 过滤自定义组件vnode
     const vnode = slot.filter((item) => {
       return (
+        // 过滤出name是DingGuaGua的组件
         (item.componentOptions && item.componentOptions.Ctor.options.name === "DingGuaGua")
         ||
+        // 过滤出name是Title组件
         (item.componentOptions && item.componentOptions.Ctor.options.name === "Title")
       );
     });
+
     // 过滤原生vnode DOM
     const dom = slot.filter(item => !item.componentOptions)
+
+    // 打印
     console.log(slot);
     return (
       <div>
+        {/* 直接渲染 */}
         { dom }
+
+        {/* 通过参数传递vnode */}
         <Describe vnodes={vnode}></Describe>
       </div>
     );
