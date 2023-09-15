f0a8eea2c213719fb4cd749d7effe0f05bdb8af0
txatxp <185389903@qq.com>
2023-09-15 01:07:24 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
@@ -9,20 +9,33 @@ Vue.use(VueRouter)
 // 或者，只是一个组件配置对象。
 // 我们晚点再讨论嵌套路由。
 const routes = [
-  { path: '/', component: () => import('../views/index.vue') },
-  { path: '/h', component: () => import('../views/h/index.vue') },
-  { path: '/jsx', component: () => import('../views/jsx/index.vue') },
-  { path: '/inline/comp', component: () => import('../views/inlineComp/index.vue') },
-  { path: '/slots/scope', component: () => import('../views/slotsScope/index.vue') },
-  { path: '/assembly/package', component: () => import('../views/assemblyPackage/index.vue') },
-  { path: '/secondary/assembly/package', component: () => import('../views/secondaryAssemblyPackage/index.vue') },
-  { path: '/el-table', component: () => import('../views/simpleTable/index.vue') },
-]
-
+  { path: "/", component: () => import("../views/index.vue") },
+  { path: "/h", component: () => import("../views/h/index.vue") },
+  { path: "/jsx", component: () => import("../views/jsx/index.vue") },
+  {
+    path: "/inline/comp",
+    component: () => import("../views/inlineComp/index.vue"),
+  },
+  {
+    path: "/slots/scope",
+    component: () => import("../views/slotsScope/index.vue"),
+  },
+  {
+    path: "/assembly/package",
+    component: () => import("../views/assemblyPackage/index.vue"),
+  },
+  {
+    path: "/secondary/assembly/package",
+    component: () => import("../views/secondaryAssemblyPackage/index.vue"),
+  },
+  {
+    path: "/simple-table",
+    component: () => import("../views/simpleTable/index.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes // (缩写) 相当于 routes: routes
-})
+  routes, // (缩写) 相当于 routes: routes
+});
 
-export default router
-
+export default router;
